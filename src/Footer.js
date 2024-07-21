53364d58302a3f1c08ecff1c22af633bf4d72627
azizaguvanjova <aziza.guvanjova13@gmail.com>
2024-07-21 11:25:37 +0300

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+const Footer = () => {
+  return (
+   <footer>
+<p> 
+ Made by <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"> Coding Company</a>™.
+</p>
+   </footer>
+  )
+}
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const Header = () => {
+  return (
+    <header>
+         <div className="button-container">
+                        <button>Menu</button> 
+                       <button>Hakkında</button> 
+                       <button>Saatler & Lokasyon</button>          
+         </div> 
+    </header>
+  )
+}
+export default Header
